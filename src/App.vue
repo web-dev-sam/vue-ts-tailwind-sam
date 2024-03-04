2cff4b07a8af8afd645629abd7d2e29887358b4e
Samuel Braun <office.samigo.a@gmail.com>
2024-03-04 15:37:37 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-console.log("test");
+console.log('test');
 </script>
 
 <template>
