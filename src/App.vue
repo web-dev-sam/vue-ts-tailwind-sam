ca3ddf2c0a5fdbbcf04a02b33aeb93b971943b6e
Samuel Braun <office.samigo.a@gmail.com>
2024-03-04 15:57:52 +0100

    Cleanup formatting

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-console.log('test', 0);
+import { RouterLink, RouterView } from "vue-router"
+import HelloWorld from "./components/HelloWorld.vue"
 </script>
 
 <template>
